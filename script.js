171910de38547f7210716206a4438aa96470be15
SGRuzo <sgarciaruzo@danielcastelao.org>
2025-05-05 12:34:12 +0200

    modificaciones del index

--- script.js
+++ script.js
@@ -1,25 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Obtener todos los botones de juego
-    const playButtons = document.querySelectorAll('.play-btn');
+    // Efecto hover para los botones
+    const botonesJuego = document.querySelectorAll('.boton-juego');
     
-    // Agregar evento click a cada botón
-    playButtons.forEach(button => {
-        button.addEventListener('click', (e) => {
-            const gameId = e.target.closest('.game-card').id;
-            switch(gameId) {
-                case 'ahorcado':
-                    window.location.href = 'Ahorcado.html';
-                    break;
-                case 'candy-crush':
-                    window.location.href = 'Cc.html';
-                    break;
-                case 'flappy-bird':
-                    window.location.href = 'fb.html';
-                    break;
-                case 'memory':
-                    window.location.href = 'memory.html';
-                    break;
-            }
+    botonesJuego.forEach(boton => {
+        boton.addEventListener('mouseenter', () => {
+            boton.style.transform = 'scale(1.1)';
+        });
+        
+        boton.addEventListener('mouseleave', () => {
+            boton.style.transform = 'scale(1)';
         });
     });
 });
